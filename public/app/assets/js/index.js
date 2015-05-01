d40d667e426a96c6701e0928e7536033bfeb0774
Wilder <redlim@gmail.com>
2015-05-01 14:24:14 +0200

    Las últimas cosas

--- public/app/assets/js/index.js
+++ public/app/assets/js/index.js
@@ -1,17 +1,21 @@
-
+$("#listaLibros").hide("slow");
 $("#mainButton").click(function(){
-  $("#listaLibros").hide("slow");
-  $.get( "/dameLibros", function(data) {
+ var autor = $("#hola").val();
+  $.get( "/getBookcase?author=" + autor, function(data) {
+    console.log(data);
     $('body').scrollspy({ target: '#listaLibros' });
-      console.log(data.libros);
-    for(var i=0; i< data.libros.length;i++){
-
-      $("#listaLibros").append('<div class="col-md-3">'+ data.libros[i].nombre + "<img class='img-responsive' src='" + data.libros[i].img + "'>"+ '</div>');
-      //$("#listaLibros").append("<img class='img-responsive' src='" + data.libros[i].img + "'>" )
+      //console.log(data.libros);
+    for(var i=0; i< 15;i++){
+      if(data[i].img!="") {
+        $("#listaLibros").append('<div class="col-md-3">' + data[i].title + "<img class='librillo img-responsive' src='" + data[i].img + "'>" + '</div>');
+        //$("#listaLibros").append("<img class='img-responsive' src='" + data.libros[i].img + "'>" )
+      }
     }
     $("#listaLibros").show("slow");
     //$("#listaLibros").show("slow");
     });
+
+  var json_awser;
 
   //<div class="row">
     //<div class="md-col-3"></div>
@@ -19,7 +23,9 @@ $("#mainButton").click(function(){
     //<div class="md-col-3"></div>
     //
     //</div>
+});
 
-
-
+$(".librillo").click(function(){
+  $(this).append('<div class="hola col-md-3"><h2>Pertenece a <small>Sergey Arellano</small></h2><br><p>Se encuentra en Móstoles City</p></div>')
+  $(".hola").show("slow");
 });
